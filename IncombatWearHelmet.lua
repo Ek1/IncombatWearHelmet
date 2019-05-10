fbc62514cd77a5df3e22226eef3ec91148c4539f
Erkki Seppä <CiO@Ek1.Fi>
2019-05-10 20:17:57 +0300

    typo

--- IncombatWearHelmet.lua
+++ IncombatWearHelmet.lua
@@ -2,7 +2,7 @@ local IncombatWearHelmet = {
 	Title = "Incombat wear helmet",
 	Author = "Ek1",
 	Description = "Shows helmet when entering combat and hides it when exiting combat",
-	Version = "190519",
+	Version = "190510",
 	License = "CC BY-SA: Creative Commons Attribution-ShareAlike 4.0 International License",
 	www = "https://github.com/Ek1/IncombatWearHelmet"
 }
