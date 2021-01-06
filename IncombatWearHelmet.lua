f895973d841d4f7f1f2929a31231aa9bdcd4e9f5
Erkki Seppä <CiO@Ek1.Fi>
2021-01-07 01:34:40 +0200

    33.210106

--- IncombatWearHelmet.lua
+++ IncombatWearHelmet.lua
@@ -2,48 +2,61 @@ local IncombatWearHelmet = {
 	Title = "Incombat wear helmet",	-- Not codereview friendly but enduser friendly version of the add-on's name
 	Author = "Ek1",
 	Description = "Shows helmet when entering combat and hides it when exiting combat",
-	Version = "32.20200926",
+	Version = "33.210106",
 	License = "CC BY-SA: Creative Commons Attribution-ShareAlike 4.0 International License",
 	www = "https://github.com/Ek1/IncombatWearHelmet"
 }
 local ADDON = "IncombatWearHelmet"	-- Variable used to refer to this add-on. Codereview friendly.
--- Funktion that changes the helmet visibility according to the combat state
-function IWH_combatState ()
 
-	local Incombat = IsUnitInCombat("player")
+-- hatswap with 
+-- GetCollectibleCooldownAndDuration(number collectibleId)
+
+-- Funktion that changes the helmet visibility according to the combat state
+function IncombatWearHelmet.combatState (inCombat)
 	-- Prepearing hat for all 2^2 state evalutions
 	local activeHat = GetActiveCollectibleByType(COLLECTIBLE_CATEGORY_TYPE_HAT)
+	local cooldownRemaining = 0
+	local cooldownDuration = 0
 
-	if Incombat then
+	if inCombat then
 		if activeHat ~= 0 then
 		-- Character is in combat and hiding helmet (activeHat=0) thus lets unhide it
 			UseCollectible(5002)
+--			cooldownRemaining, cooldownDuration = GetCollectibleCooldownAndDuration(5002)
+--			zo_callLater(function() IWH_combatState (IsUnitInCombat("player") ) end, cooldownRemaining )
 		end
 	else
 		if activeHat ~= 5002 then
 		-- Character is not in combat and showing helmet (activeHat=5002) thus lets hide it
-            UseCollectible(5002)
+			UseCollectible(5002)
+--			cooldownRemaining, cooldownDuration = GetCollectibleCooldownAndDuration(5002)
+--			zo_callLater(function() IncombatWearHelmet.combatState (IsUnitInCombat("player") ) end, cooldownRemaining )
 		end
 	end
 end
 
+-- 100033	EVENT_PLAYER_COMBAT_STATE (number eventCode, boolean inCombat)
+function IncombatWearHelmet.EVENT_PLAYER_COMBAT_STATE (_, inCombat)
+	IncombatWearHelmet.combatState(inCombat)
+end
+
+function IncombatWearHelmet.EVENT_ZONE_CHANGED (_, _)
+	IncombatWearHelmet.combatState(	IsUnitInCombat("player")	)
+end
+
 -- Lets fire up the add-on by registering for events
 function IncombatWearHelmet.Initialize()
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_PLAYER_COMBAT_STATE, IWH_combatState)	-- listening when entering/exiting combat
-	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_ZONE_CHANGED, IWH_combatState)	-- listening when zone changes
+	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_PLAYER_COMBAT_STATE, IncombatWearHelmet.EVENT_PLAYER_COMBAT_STATE)	-- listening when entering/exiting combat
+	EVENT_MANAGER:RegisterForEvent(ADDON, EVENT_ZONE_CHANGED, IncombatWearHelmet.EVENT_ZONE_CHANGED)	-- listening when zone changes
 	--d( IncombatWearHelmet.Title .. ": initalization done")
 end
 
--- Variable to keep count how many loads have been done before it was this ones turn.
-local loadOrder = 1
-function IncombatWearHelmet.OnAddOnLoaded(event, addonName)
-  if addonName == ADDON then
---	Seems it is our time so lets stop listening load trigger and initialize the add-on
-	--d( IncombatWearHelmet.Title .. ": load order " ..  loadOrder .. ", starting initalization")
-	EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_ADD_ON_LOADED)
-	IncombatWearHelmet.Initialize()
-  end
-  loadOrder = loadOrder+1
+-- Listener to know when its our turn.
+function IncombatWearHelmet.OnAddOnLoaded(_, addonName)
+  if addonName == ADDON then	-- Its our time so lets stop listening load trigger and initialize the add-on
+		EVENT_MANAGER:UnregisterForEvent(ADDON, EVENT_ADD_ON_LOADED)
+		IncombatWearHelmet.Initialize()
+	end
 end
 
 -- Registering the addon's initializing event when add-on's are loaded 
