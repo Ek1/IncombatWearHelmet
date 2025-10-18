b4d3986fbdcfe77ed8fc28fccc152518e824b885
Ek1 <seppa.erkki@gmail.com>
2025-10-18 19:09:28 +0300

    API bump

--- IncombatWearHelmet.lua
+++ IncombatWearHelmet.lua
@@ -2,7 +2,7 @@ local IncombatWearHelmet = {
 	Title = "Incombat wear helmet",	-- Not codereview friendly but enduser friendly version of the add-on's name
 	Author = "Ek1",
 	Description = "Shows helmet when entering combat and hides it when exiting combat",
-	Version = "1044.241122",
+	Version = "1047.251018",
 	License = "CC BY-SA: Creative Commons Attribution-ShareAlike 4.0 International License",
 	www = "https://github.com/Ek1/IncombatWearHelmet"
 }
